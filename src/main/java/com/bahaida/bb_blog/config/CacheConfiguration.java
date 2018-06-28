d50faf6a4e61d87614ba387a7beb2ce5dcd002b2
Brahim Bahaida <bbahieda@gmail.com>
2018-06-28 08:54:36 +0000

    Add blog, entry, tag entities

--- src/main/java/com/bahaida/bb_blog/config/CacheConfiguration.java
+++ src/main/java/com/bahaida/bb_blog/config/CacheConfiguration.java
@@ -38,6 +38,11 @@ public class CacheConfiguration {
             cm.createCache(com.bahaida.bb_blog.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.bahaida.bb_blog.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.bahaida.bb_blog.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.bahaida.bb_blog.domain.Blog.class.getName(), jcacheConfiguration);
+            cm.createCache(com.bahaida.bb_blog.domain.Entry.class.getName(), jcacheConfiguration);
+            cm.createCache(com.bahaida.bb_blog.domain.Entry.class.getName() + ".tags", jcacheConfiguration);
+            cm.createCache(com.bahaida.bb_blog.domain.Tag.class.getName(), jcacheConfiguration);
+            cm.createCache(com.bahaida.bb_blog.domain.Tag.class.getName() + ".entries", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { BbBlogBlogModule } from './blog/blog.module';
+import { BbBlogEntryModule } from './entry/entry.module';
+import { BbBlogTagModule } from './tag/tag.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        BbBlogBlogModule,
+        BbBlogEntryModule,
+        BbBlogTagModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
